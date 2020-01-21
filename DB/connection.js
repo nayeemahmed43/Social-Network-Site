df5297fad6fd3908053679258f6e2bcfa848ccef
unknown <you@example.com>
2020-01-21 10:21:11 +0600

    changed

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-//const URI = "mongodb+srv://useradmin:dbadpassword@cluster0-l6rwf.mongodb.net/test?retryWrites=true&w=majority";
+const URI = "mongodb+srv://useradmin:dbadpassword@cluster0-l6rwf.mongodb.net/test?retryWrites=true&w=majority";
 
 const connectDB = async() =>{
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-site', {
+    await mongoose.connect(process.env.MONGODB_URI || URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true
         });
